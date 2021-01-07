e4557702ec0b50912e9ead6dee33471869bed645
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2021-01-07 13:28:11 +0800

    Fix: test files should not be included in coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'test_frameworks'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'huffman_coding'
-
 require 'minitest/autorun'
